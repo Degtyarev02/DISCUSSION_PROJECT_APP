b670dae1c9c1077f2c716e88a446bd95edad34ad
Vladimir <vladimirdegty@gmail.com>
2021-04-26 20:11:38 +0300

    saveImage

--- Messenger_Project/app/src/main/java/com/example/messenger_project/Activities/ShowImageActivity.java
+++ Messenger_Project/app/src/main/java/com/example/messenger_project/Activities/ShowImageActivity.java
@@ -1,16 +1,42 @@
 package com.example.messenger_project.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Environment;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.ImageView;
 
+import com.example.flatdialoglibrary.dialog.FlatDialog;
 import com.example.messenger_project.R;
+import com.ortiz.touchview.TouchImageView;
 import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Random;
+
+import es.dmoral.toasty.Toasty;
 
 public class ShowImageActivity extends AppCompatActivity {
 
+    private BitmapDrawable drawable;
+    private Bitmap bitmap;
 
 
     @Override
@@ -21,7 +47,99 @@ public class ShowImageActivity extends AppCompatActivity {
         Intent i = getIntent();
         String image = i.getStringExtra("image");
 
-        ImageView imageView = findViewById(R.id.imageView);
+        TouchImageView imageView = findViewById(R.id.imageView);
         Picasso.get().load(image).into(imageView);
+
+        imageView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                FlatDialog flatDialog = new FlatDialog(ShowImageActivity.this);
+                flatDialog
+                        .setTitle("Save Image?")
+                        .setTitleColor(getResources().getColor(R.color.Gray))
+                        .setFirstButtonText("Save")
+                        .setSecondButtonText("Cancel")
+                        .setBackgroundColor(getResources().getColor(R.color.white))
+                        .setFirstButtonColor(getResources().getColor(R.color.ReallyGray))
+                        .setSecondButtonColor(getResources().getColor(R.color.Gray))
+                        .setFirstButtonTextColor(getResources().getColor(R.color.blackyGray))
+                        .setSecondButtonTextColor(getResources().getColor(R.color.whity_gray))
+                        .withFirstButtonListner(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                {
+                                    Drawable drawable = imageView.getDrawable();
+                                    bitmap = ((BitmapDrawable) drawable).getBitmap();
+                                    try {
+                                        CheckPermission();
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                    flatDialog.dismiss();
+                                }
+                            }
+                        })
+                        .withSecondButtonListner(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                flatDialog.dismiss();
+                            }
+                        })
+                        .show();
+                return true;
+            }
+        });
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 112)
+        {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    try {
+                        saveToInternalStorage();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else
+                {
+                    try {
+                        saveToInternalStorage();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return;
+        }
+    }
+
+    private void saveToInternalStorage() throws IOException {
+
+            File filePath = Environment.getExternalStorageDirectory();
+            File dir = new File(filePath.getAbsolutePath());
+            dir.mkdir();
+            File file = new File(dir, System.currentTimeMillis() + ".jpg");
+            FileOutputStream outputStream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+            Toasty.success(getApplication(), "Image Save to " + dir, Toasty.LENGTH_SHORT).show();
+            outputStream.flush();
+            outputStream.close();
+    }
+
+    private void CheckPermission() throws IOException {
+        int permissionStatus = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
+            saveToInternalStorage();
+        } else {
+            ActivityCompat.requestPermissions(
+                    ShowImageActivity.this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    112);
+        }
+
     }
 }
